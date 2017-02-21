c42588ac5da5973465999fd9f9eebe7b07849cc7
Areeb Beigh <areebbeigh@gmail.com>
2017-02-22 01:34:36 +0530

    Improvements

--- animejoy.tv/prepare_download_list.py
+++ animejoy.tv/prepare_download_list.py
@@ -24,23 +24,26 @@ ep_number = 1
 hash_map = {}
 
 for url in websites:
-    source = scraper.get(url).content
-    soup = bs(source, "html.parser")
-    scripts = soup.find("div", {"id": "video_container_div"}).find_all("script")
-    for script in scripts:
-        download_url = ""
-        try:
-            download_url = re.search(
-            r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*.mp4)",
-            str(script)).group()
-        except AttributeError:
-            continue
+    try:
+        source = scraper.get(url).content
+        soup = bs(source, "html.parser")
+        scripts = soup.find("div", {"id": "video_container_div"}).find_all("script")
+        for script in scripts:
+            download_url = ""
+            try:
+                download_url = re.search(
+                r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*.mp4)",
+                str(script)).group()
+            except AttributeError:
+                continue
 
-        if download_url:
-            ep = "Episode " + str(ep_number)
-            print(ep + ": " + download_url)
-            downloads.append(download_url)
-            hash_map[download_url] = ep
+            if download_url:
+                ep = "Episode " + str(ep_number)
+                print(ep + ": " + download_url)
+                downloads.append(download_url)
+                hash_map[download_url] = ep
+    except:
+        print("Failed to get Episode " + str(ep_number))
 
     ep_number += 1
 
